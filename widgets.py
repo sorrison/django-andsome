84d94885eb647bbb1bf9cbc4fcc895db4d2c833d
Sam Morrison <sam@vpac.org>
2008-04-24 02:24:02 +0000

    radio inline widget

--- widgets.py
+++ widgets.py
@@ -1,10 +1,12 @@
 from django.newforms import *
-from django.newforms.widgets import flatatt
+#from django.newforms.widgets import RadioInput
 from django.newforms.util import smart_unicode
-from django.utils.html import escape
+from django.utils.html import escape, conditional_escape
 from django.utils.simplejson import *
 from django.utils.safestring import mark_safe   
-
+from django.utils.encoding import StrAndUnicode, force_unicode
+from django.utils.safestring import mark_safe
+from django.newforms.util import flatatt
 
 class TinyMCE(Textarea):
     """
@@ -35,3 +37,60 @@ class TinyMCE(Textarea):
         
         return mark_safe(u'<textarea%s>%s</textarea> <script type="text/javascript">\
                 tinyMCE.init(%s)</script>' % (flatatt(final_attrs), escape(value), mce_json))
+
+
+
+class RadioInput(StrAndUnicode):
+    """
+    An object used by RadioFieldRenderer that represents a single
+    <input type='radio'>.
+    """
+
+    def __init__(self, name, value, attrs, choice, index):
+        self.name, self.value = name, value
+        self.attrs = attrs
+        self.choice_value = force_unicode(choice[0])
+        self.choice_label = force_unicode(choice[1])
+        self.index = index
+
+    def __unicode__(self):
+        return mark_safe(u'%s<label for=%s>%s</label>' % (self.tag(), self.attrs['id'],
+                conditional_escape(force_unicode(self.choice_label))))
+
+    def is_checked(self):
+        return self.value == self.choice_value
+
+    def tag(self):
+        if 'id' in self.attrs:
+            self.attrs['id'] = '%s_%s' % (self.attrs['id'], self.index)
+        final_attrs = dict(self.attrs, type='radio', name=self.name, value=self.choice_value)
+        if self.is_checked():
+            final_attrs['checked'] = 'checked'
+        return mark_safe(u'<input%s />' % flatatt(final_attrs))
+
+
+
+class InlineRadioFieldRenderer(StrAndUnicode):
+    """
+    An object used by RadioSelect to enable customization of radio widgets.
+    """
+
+    def __init__(self, name, value, attrs, choices):
+        self.name, self.value, self.attrs = name, value, attrs
+        self.choices = choices
+
+    def __iter__(self):
+        for i, choice in enumerate(self.choices):
+            yield RadioInput(self.name, self.value, self.attrs.copy(), choice, i)
+
+    def __getitem__(self, idx):
+        choice = self.choices[idx] # Let the IndexError propogate
+        return RadioInput(self.name, self.value, self.attrs.copy(), choice, idx)
+
+    def __unicode__(self):
+        return self.render()
+
+    def render(self):
+        """Outputs a <ul> for this set of radio fields."""
+        return mark_safe(u'<ul class="radiolist inline">\n%s\n</ul>' % u'\n'.join([u'<li>%s</li>'
+                % force_unicode(w) for w in self]))
