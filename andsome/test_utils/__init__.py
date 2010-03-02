ccfcb13e970d7eb8e823f34f4c55e2d54732de37
Sam Morrison <sam@vpac.org>
2010-03-02 11:16:42 +1100

    Fixed graph_lib bug

--- andsome/test_utils/__init__.py
+++ andsome/test_utils/__init__.py
@@ -14,21 +14,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with django-andsome  If not, see <http://www.gnu.org/licenses/>.
-
-
-from django.conf import settings
-import datetime
-
-module = __import__(settings.GRAPH_LIB, {}, {}, [''])
-
-grapher = module.GraphGenerator()
-
-
-def gen_sparkline():
-    return grapher.sparkline()
-
-def bar_chart(data, labels, x_labels, max_y):
-    return grapher.bar_chart(data, labels, x_labels, max_y)
-
-def pie_chart(data_dict):
-    return grapher.pie_chart(data_dict)
