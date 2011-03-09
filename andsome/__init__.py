e74669d4203802d5a66830f05ecb12e7ef5bf16b
Sam Morrison <sam@vpac.org>
2011-03-09 13:09:49 +1100

    New version 1.1.8

--- andsome/__init__.py
+++ andsome/__init__.py
@@ -16,7 +16,7 @@
 # along with django-andsome  If not, see <http://www.gnu.org/licenses/>.
 
 
-VERSION = (1, 1, 7, 'final', 0)
+VERSION = (1, 1, 8, 'final', 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
